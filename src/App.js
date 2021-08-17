e9b082e8e5f1964ea468add33b83e9b8d8251a43
skalshetty57 <74254221+skalshetty57@users.noreply.github.com>
2021-08-17 20:51:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// import react from 'React';
+
+function App() {
+  return <h1>hello</h1>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+import "react-bootstrap";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
